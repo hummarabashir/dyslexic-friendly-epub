cb1866496b0a91e175baf111d0f2a129b3aa5558
Hummara Bashir <hummarabashir@gmail.com>
2023-09-18 14:35:23 -0400

    epubs

--- OEBPS/js/script.js
+++ OEBPS/js/script.js
@@ -1,7 +1,10 @@
 document
   .getElementById("dyslexia-mode-toggle")
   .addEventListener("click", function () {
-    document.body.classList.toggle("dyslexia-mode");
+    // document.body.classList.toggle("dyslexia-mode");
+    document.body.style.fontFamily = "font-family: OpenDyslexic, Georgia, 'Times New Roman', Times, serif;";
+
+    // document.getElementById("div1").classList.toggle("dyslexia-mode");
     // Store the dyslexia-friendly mode state in local storage
     var dyslexiaModeEnabled = document.body.classList.contains("dyslexia-mode");
     localStorage.setItem("dyslexiaMode", dyslexiaModeEnabled);
